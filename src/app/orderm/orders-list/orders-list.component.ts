efaacee76fb37f8d21690aa7609b151892ba8a56
Omkar <omkar.nagvekar@igp.com>
2017-02-28 18:48:51 +0530

    Develop Order component

--- src/app/orderm/orders-list/orders-list.component.ts
+++ src/app/orderm/orders-list/orders-list.component.ts
@@ -6,11 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders-list.component.css']
 })
 export class OrdersListComponent implements OnInit {
+  allOrders = [
+    {id: '000001', description: 'Order 1', status: 'Active', date: '11 January 2017'},
+    {id: '000002', description: 'Order 2', status: 'Active', date: '15 January 2017'},
+    {id: '000003', description: 'Order 3', status: 'Active', date: '15 January 2017'},
+    {id: '000004', description: 'Order 4', status: 'Active', date: '22 January 2017'},
+    {id: '000005', description: 'Order 5', status: 'Active', date: '26 January 2017'},
+    {id: '000006', description: 'Order 6', status: 'Active', date: '05 February 2017'},
+    {id: '000007', description: 'Order 7', status: 'Active', date: '12 February 2017'},
+    {id: '000008', description: 'Order 8', status: 'Active', date: '18 February 2017'},
+    {id: '000009', description: 'Order 9', status: 'Active', date: '21 February 2017'},
+    {id: '000010', description: 'Order 10', status: 'Active', date: '24 February 2017'}
+  ];
 
   constructor() { }
 
   ngOnInit() {
-    console.log('OrdersListComponent called successfully');    
+    console.log('OrdersListComponent called successfully', this.allOrders);
   }
 
 }
