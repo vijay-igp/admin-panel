6fd41db39484c1ce8fdd6c5b653497213e639477
Omkar <omkar.nagvekar@igp.com>
2017-03-01 18:16:05 +0530

    Integrate basic authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -6,20 +7,32 @@ import { DropdownModule } from 'ng2-bootstrap';
 import { SelectModule } from 'ng2-select';
 import { OrdermModule } from './orderm/orderm.module';
 
-import { routing } from "./app.routing";
+// used to create fake backend
+import { fakeBackendProvider } from 'helpers/fake-backend';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
 
+// Router and Services
+import { routing } from "./app.routing";
 import { MetricService } from 'services/metric.service';
 import { ProductService } from 'services/product.service';
 import { BackendService } from 'services/backend.service';
 import { LotDataService } from 'services/lot-data.service';
 import { Logger } from 'services/logger.service';
+import { AuthenticationService } from 'services/authentication.service';
+import { AuthGuard } from 'services/auth-guard.service';
+import { UserService } from 'services/user.service';
 
+// Components
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { MetricsListComponent } from './metrics-list/metrics-list.component';
 import { LotRowComponent } from './lot-row/lot-row.component';
 import { FormValidationComponent } from './form/form.component';
 import { LotDetailsComponent } from './lot-details/lot-details.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +41,10 @@ import { LotDetailsComponent } from './lot-details/lot-details.component';
     MetricsListComponent,
     LotRowComponent,
     FormValidationComponent,
-    LotDetailsComponent
+    LotDetailsComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +60,15 @@ import { LotDetailsComponent } from './lot-details/lot-details.component';
     MetricService,
     ProductService,
     LotDataService,
-    Logger
+    Logger,
+    AuthenticationService,
+    AuthGuard,
+    UserService,
+
+    // providers used to create fake backend
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
   ],
   bootstrap: [AppComponent]
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { MetricsListComponent } from './metrics-list/metrics-list.component';
 import { FormValidationComponent } from './form/form.component';
 import { OrdersListComponent } from './orderm/orders-list/orders-list.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+import { AuthGuard } from 'services/auth-guard.service';
 
 const route: Routes = [
-    { path: '', redirectTo: 'product_upload', pathMatch: 'full' },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'product_upload', component: MetricsListComponent },
     { path: 'form', component: FormValidationComponent },
-    { path: 'orders', component: OrdersListComponent }
+    { path: 'orders', component: OrdersListComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
 ]
 
 export const routing = RouterModule.forRoot(route);
