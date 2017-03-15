45ca4db7ba5ab1d2edbdc362899219bd38a82ae3
Omkar <omkar.nagvekar@igp.com>
2017-03-15 18:53:59 +0530

    Integrate file upload with drop

--- src/app/orderm/orderm.module.ts
+++ src/app/orderm/orderm.module.ts
@@ -7,16 +7,21 @@ import { OrdersListComponent } from './orders-list/orders-list.component';
 import { OrdersComponent } from './orders/orders.component';
 import { OrderDetailsComponent } from './order-details/order-details.component';
 
+import { FileUploadModule } from "ng2-file-upload";
+import { XlsxFileUploadComponent } from './xlsx-file-upload/xlsx-file-upload.component';
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    FileUploadModule
   ],
   declarations: [
     OrdersListComponent,
     OrdersComponent,
-    OrderDetailsComponent
+    OrderDetailsComponent,
+    XlsxFileUploadComponent
   ]
 })
 export class OrdermModule { }
