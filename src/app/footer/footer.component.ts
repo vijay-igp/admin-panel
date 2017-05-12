0714263a5b79c31026a403c7ce229b0a34bd049e
Omkar <omkar.nagvekar@igp.com>
2017-05-12 16:50:16 +0530

    Modify footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,8 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  template: `<div class="row footer">
+                <p>&copy; IGP.com - Intermesh Pvt Ltd. - 2017</p>
+            </div>`,
+  styles: [`.footer{
+              bottom:0;
+              text-align: center;
+              text-decoration: underline;
+              margin: 10px 0 0;
+              width:100%;
+          }`]
 })
 export class FooterComponent implements OnInit {
 
